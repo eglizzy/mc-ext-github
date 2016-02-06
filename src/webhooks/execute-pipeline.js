117a95848e4999a72e465a16dc22d7fac7d84efe
Andy Fleming <andy@andyfleming.com>
2016-02-06 14:39:46 -0800

    Adding notes about future github event filtering
    (and noting example URL)

--- src/webhooks/execute-pipeline.js
+++ src/webhooks/execute-pipeline.js
@@ -7,6 +7,7 @@ function githubSignatureIsValid(secret, blob, signature) {
 
 module.exports = {
 
+  // https://your-mission-control.com/ext/mc/github/webhooks/execute-pipeline/123
   route: 'execute-pipeline/:id',
 
   handler: (req, res, helper) => {
@@ -25,6 +26,15 @@ module.exports = {
     }
 
     console.log('github sig valid')
+
+    // TODO: check if this pipeline_id has github webhooks enabled
+    // let settings = helper.pipelineSettings(pipeline_id, 'mc-ext-github')
+    // if (settings.events.whatever === 1)
+    // TODO: if not return 403/200/whatever
+
+
+    // TODO: filter events based on extension/pipeline settings
+    // if (req.body.event === 'pull_request.comment')
 
     try {
       helper.executePipeline(req.params.id, {webhookData: req.body}, () => {
