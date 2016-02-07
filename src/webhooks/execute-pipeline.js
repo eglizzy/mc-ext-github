f1a6e8e8c078b5372aac578ce3ed6a8adfc8ac20
Elijah McPherson <emcpherson@undergroundelephant.com>
2016-02-06 21:12:59 -0800

    Adds pipeline settings

--- src/webhooks/execute-pipeline.js
+++ src/webhooks/execute-pipeline.js
@@ -6,7 +6,6 @@ function githubSignatureIsValid(secret, blob, signature) {
 }
 
 module.exports = {
-
   // https://your-mission-control.com/ext/mc/github/webhooks/execute-pipeline/123
   route: 'execute-pipeline/:id',
 
@@ -15,9 +14,6 @@ module.exports = {
     let reqBody = req.rawBody || ''
     let providedSignature = req.headers['x-hub-signature'] || ''
 
-    console.log('received github hook')
-    console.dir(req)
-
     if (!githubSignatureIsValid(process.env.GITHUB_WEBHOOK_SECRET, reqBody, providedSignature)) {
       console.log('github sig invalid')
       res.status(403)
@@ -25,146 +21,52 @@ module.exports = {
       return
     }
 
-    console.log('github sig valid')
 
-    // TODO: check if this pipeline_id has github webhooks enabled
-    // let settings = helper.pipelineSettings(pipeline_id, 'mc-ext-github')
-    // if (settings.events.whatever === 1)
-    // TODO: if not return 403/200/whatever
+    const event = req.headers['x-github-event']
+    const action = req.body.action || ''
 
+    const settings = helper.pipelineSettings(req.params.id, 'mc', 'github')
 
-    // TODO: filter events based on extension/pipeline settings
-    // if (req.body.event === 'pull_request.comment')
-
-    // Potential extension data structure
-    //config.extension_settings = {
-    //  mc: {
-    //    github: {
-    //      webhook_enabled: 1,
-    //      event_watch_started_enabled: 1
-    //    }
-    //  }
-    //}
+    if (settings.hasOwnProperty('webhook')) {
+      if (!settings.webhook.enabled) {
+        res.status(500)
+        return res.send()
+      }
 
 
-    // - Event types ----------------------------
+      const hooks = settings.webhook;
+      const key = event + '_' + action
+      if ('pull_request_closed' == key) {
+        if (hooks[key] && !payload.merged) {
+          res.status(200)
+          return res.send()
+        }
+      }
+      if (hooks[key]) {
+        try {
+          return helper.executePipeline(req.params.id, {webhookData: req.body}, () => {
+            res.send()
+          })
+        } catch (err) {
+          console.error(err)
+          res.status(500)
+          return res.send()
+        }
+      }
 
-    // Event Name
-    //    Webhook event name
-    //        Action
-
-    // CreateEvent
-    //    commit_comment
-    //        created
-
-    // DeleteEvent
-    //    delete
-
-    // DeploymentEvent
-    //    deployment
-
-    // DeploymentStatusEvent
-    //    deployment_status
-
-    // DownloadEvent
-    //    download
-
-    // FollowEvent
-    //    follow
-
-    // ForkEvent
-    //    fork
-
-    // ForkApplyEvent
-    //    fork_apply
-
-    // GistEvent
-    //    gist
-
-    // GollumEvent
-    //    gollum
-
-    // IssueCommentEvent
-    //    issue_comment
-    //        created
-
-    // IssuesEvent
-    //    issues
-    //        assigned
-    //        unassigned
-    //        labeled
-    //        unlabeled
-    //        opened
-    //        closed
-    //        reopened
-
-    // MemberEvent
-    //    member
-    //        added
-
-    // MembershipEvent
-    //    membership
-    //        added
-    //        removed
-
-    // PageBuildEvent
-    //    page_build
-
-    // PublicEvent
-    //    public
-
-    // PullRequestEvent
-    //    action
-    //        assigned
-    //        unassigned
-    //        labeled
-    //        unlabeled
-    //        opened
-    //        closed
-    //        reopened
-    //        synchronize
-    //
-    // If the action is "closed" and the merged key is false, the pull request was closed with unmerged commits.
-    // If the action is "closed" and the merged key is true, the pull request was merged.
-
-    // PullRequestReviewCommentEvent
-    //    pull_request_review_comment
-    //       created
-
-    // PushEvent
-    //    push
-
-    // ReleaseEvent
-    //    release
-    //        published
-
-    // RepositoryEvent
-    //    repository
-    //        created
-
-    // StatusEvent
-    //    status
-
-    // TeamAddEvent
-    //    team_add
-
-    // WatchEvent
-    //    watch
-    //        started
-
-
-
-    try {
-      helper.executePipeline(req.params.id, {webhookData: req.body}, () => {
-        res.send()
-      })
-
-    } catch(err) {
-      console.error(err)
-      res.status(500)
-      res.send()
+      res.status(422)
+      return res.send()
     }
 
+    try {
+      return helper.executePipeline(req.params.id, {webhookData: req.body}, () => {
+        res.send()
+      })
+    } catch (err) {
+      console.error(err)
+      res.status(500)
+      returnres.send()
+    }
   }
 
 }
